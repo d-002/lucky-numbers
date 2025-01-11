5c074c1fc410a824f6cee3cc4268505e40e6aa63
d-002 <minecraftenjoy@outlook.fr>
2025-01-11 18:25:52 +0100

    added check functions for common tiles

--- cli/player.c
+++ cli/player.c
@@ -4,14 +4,34 @@
 
 typedef int (*check_func)(Game *, int);
 
-int in_hidden(Game *game, int n) {
-    if (n < 1 || n > 20) return 0;
+int get_card_with_condition(Game *game, int value, int isVisible) {
+    // intern function, used for check functions
+    if (value < 1 || value > 20) return 0;
 
-    return 1;
+    for (int i = 0; i < game->numTiles; i++) {
+        Tile *tile = game->allTiles+i;
+
+        if (tile->value != value) continue;
+        if (tile->isTaken) continue;
+        if (tile->isVisible == isVisible) return 1;
+    }
+
+    return 0;
+}
+
+int check_in_hidden(Game *game, int value) {
+    return get_card_with_condition(game, value, 0);
+}
+
+int check_in_visible(Game *game, int value) {
+    return get_card_with_condition(game, value, 1);
+}
+
+int check_correct_pos(Game *game, int value) {
+    return value >= 0 && value <= 3;
 }
 
 int pick_number(char *msg, Game *game, check_func func) {
-
     int n;
     while (1) {
         printf("%s", msg);
@@ -24,5 +44,5 @@ int pick_number(char *msg, Game *game, check_func func) {
 }
 
 void player_play(Game *game) {
-    pick_number("Choose a number: ", game, in_hidden);
+    pick_number("Choose a number: ", game, check_in_hidden);
 }
